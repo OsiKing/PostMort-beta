953c623aab895c6943c4290d13b2cd5588e62143
Callistus Enekhaze <44670996+OsiKing@users.noreply.github.com>
2020-07-14 18:26:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include ('templates/header.php');?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PortMort</title>
     <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css">
     <script src="./assets/bootstrap/js/bootstrap.min.js"></script>
